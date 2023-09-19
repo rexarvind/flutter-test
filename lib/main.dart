bb3f8ae65a6a3f4f787cb1179862783df0da5b17
Arvind Kumar <rexarvind.in@gmail.com>
2023-09-19 09:53:48 +0530

    add google_fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import './pages/home_page.dart';
 import './pages/settings_page.dart';
 // import './auth_service.dart';
 
 // AuthService authService = AuthService();
+const kThemeKey = 'theme_key';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final String theme = prefs.getString(kThemeKey) ?? 'system';
+
   runApp(
-    const ProviderScope(
-      child: MainApp(),
+    ProviderScope(
+      child: MainApp(theme: theme,),
     ),
   );
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, required this.theme});
+  final String theme;
 
   @override
   Widget build(BuildContext context) {
     ThemeMode getSavedTheme() {
-    // Load the saved theme from SharedPreferences.
-    // If it doesn't exist or is not set, return the default theme.
-    // You can customize your default theme here.
-    return ThemeMode.dark;
-  }
-    
+      if(theme == 'dark'){
+        return ThemeMode.dark;
+      } else if(theme == 'light'){
+        return ThemeMode.light;
+      } else {
+        return ThemeMode.system;
+      }
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: getSavedTheme(),
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        useMaterial3: true,
+        textTheme: GoogleFonts.interTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       darkTheme: ThemeData.dark(useMaterial3: true),
       home: const HomePage(),
       onGenerateRoute: (RouteSettings routeSettings) {
         return MaterialPageRoute<void>(
-          settings: routeSettings,
-          builder: (BuildContext context){
-          switch (routeSettings.name){
-            case SettingsPage.routeName:
-              return const SettingsPage();
-            default:
-              return const HomePage();
-          }
-        });
+            settings: routeSettings,
+            builder: (BuildContext context) {
+              switch (routeSettings.name) {
+                case SettingsPage.routeName:
+                  return const SettingsPage();
+                default:
+                  return const HomePage();
+              }
+            });
       },
     );
   }
--- lib/widgets/settings/theme_toggle.dart
+++ lib/widgets/settings/theme_toggle.dart
@@ -20,16 +20,7 @@ class _ThemeToggleState extends State<ThemeToggle> {
     });
   }
 
-  _changeTheme(String theme) {
-    setState(() {
-      _selectedTheme = theme;
-    });
-    _saveSelectedTheme(theme);
-  }
-
-  _saveSelectedTheme(String theme) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_themeKey, theme);
+  void _savedTheme(String theme) {
     setState(() {
       _selectedTheme = theme;
     });
@@ -52,106 +43,108 @@ class _ThemeToggleState extends State<ThemeToggle> {
         showModalBottomSheet(
             context: context,
             builder: (BuildContext context) {
-              return SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(height: 16),
-                    const Text('Choose theme'),
-                    const SizedBox(height: 4),
-                    RadioListTile(
-                      value: 'system',
-                      title: const Text('System'),
-                      groupValue: _selectedTheme,
-                      onChanged: (value) {
-                        _changeTheme(value ?? 'system');
-                      },
-                    ),
-                    RadioListTile(
-                      value: 'light',
-                      title: const Text('Light'),
-                      groupValue: _selectedTheme,
-                      onChanged: (value) {
-                        _changeTheme(value ?? 'light');
-                      },
-                    ),
-                    RadioListTile(
-                      value: 'dark',
-                      title: const Text('Dark'),
-                      groupValue: _selectedTheme,
-                      onChanged: (value) {
-                        _changeTheme(value ?? 'dark');
-                      },
-                    ),
-                    const SizedBox(height: 4),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text('Cancel'),
-                        ),
-                        const SizedBox(width: 8),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text('Save'),
-                        ),
-                        const SizedBox(width: 20),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                  ],
-                ),
+              return ThemeToggleSheet(
+                themeKey: _themeKey,
+                currentTheme: _selectedTheme,
+                savedTheme: _savedTheme,
               );
             });
-
-        /*
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return Dialog(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(height: 16),
-                    RadioListTile(
-                      value: 'system',
-                      title: const Text('System'),
-                      groupValue: _selectedTheme,
-                      onChanged: (value) {
-                        _changeTheme(value ?? 'system');
-                      },
-                    ),
-                    RadioListTile(
-                      value: 'light',
-                      title: const Text('Light'),
-                      groupValue: _selectedTheme,
-                      onChanged: (value) {
-                        _changeTheme(value ?? 'light');
-                      },
-                    ),
-                    RadioListTile(
-                      value: 'dark',
-                      title: const Text('Dark'),
-                      groupValue: _selectedTheme,
-                      onChanged: (value) {
-                        _changeTheme(value ?? 'dark');
-                      },
-                    ),
-                    const SizedBox(height: 16),
-                  ],
-                ),
-              ),
-            );
-          },
-        );
-        */
       },
+    );
+  }
+}
+
+class ThemeToggleSheet extends StatefulWidget {
+  const ThemeToggleSheet(
+      {super.key, required this.themeKey, required this.currentTheme, required this.savedTheme});
+
+  final String themeKey;
+  final String currentTheme;
+  final Function savedTheme;
+
+  @override
+  State<ThemeToggleSheet> createState() => _ThemeToggleSheetState();
+}
+
+class _ThemeToggleSheetState extends State<ThemeToggleSheet> {
+  String get themeKey => widget.themeKey;
+  String _selectedTheme = 'system';
+
+  _changeTheme(String theme) {
+    setState(() {
+      _selectedTheme = theme;
+    });
+  }
+
+  _saveSelectedTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(themeKey, _selectedTheme);
+    widget.savedTheme(_selectedTheme);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedTheme = widget.currentTheme;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 16),
+          const Text('Choose theme'),
+          const SizedBox(height: 4),
+          RadioListTile(
+            value: 'system',
+            title: const Text('System'),
+            groupValue: _selectedTheme,
+            onChanged: (value) {
+              _changeTheme(value ?? 'system');
+            },
+          ),
+          RadioListTile(
+            value: 'light',
+            title: const Text('Light'),
+            groupValue: _selectedTheme,
+            onChanged: (value) {
+              _changeTheme(value ?? 'light');
+            },
+          ),
+          RadioListTile(
+            value: 'dark',
+            title: const Text('Dark'),
+            groupValue: _selectedTheme,
+            onChanged: (value) {
+              _changeTheme(value ?? 'dark');
+            },
+          ),
+          const SizedBox(height: 4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _saveSelectedTheme();
+                },
+                child: const Text('Save'),
+              ),
+              const SizedBox(width: 20),
+            ],
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
